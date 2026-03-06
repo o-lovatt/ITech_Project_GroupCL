811f31af5ec47abe5e2a82a5436e15237505bcca
“Oliver <oliver.joe.lovatt@gmail.com>
2026-03-06 15:11:34 +0000

    extra rooms migration to try and fix admin error

--- bookings/tests.py
+++ bookings/tests.py
@@ -12,7 +12,7 @@ class BookeingTest(TestCase):
     def setUp(self):
         self.user = user_model.objects.create_user(username = "ollie", password = "password")
         self.room_type = RoomType.objects.create(room_type_name = "Family", price_per_night = 120.00, max_occupancy = 4)
-        self.room = Room.objects.create(room_number = "002", room_status = "maintenance", room_type = self.room_type)
+        self.room = Room.objects.create(room_number = "002", room_status = "available", room_type = self.room_type)
 
     #test booking works at all
     def test_valid_booking(self):#CHECK THIS AGAIN
@@ -45,6 +45,25 @@ class BookeingTest(TestCase):
         with self.assertRaises(ValidationError):
             test_booking2.clean()
         
+
+
+    def test_cancelled_booking_excluded(self):
+        #cancelled
+        test_cancelled_booking = Booking.objects.create(user = self.user, room = self.room, check_in_date = date(2026, 3, 3), check_out_date = date(2026, 3, 10), booking_status = "cancelled")#if this is reserved, the test fails
+        #reserved
+        test_reserved_booking = Booking.objects.create(user = self.user, room = self.room, check_in_date = date(2026, 3, 3), check_out_date = date(2026, 3, 10), booking_status = "reserved")
+
+        test_reserved_booking.clean()
+        test_reserved_booking.save()
+        self.assertEqual(Booking.objects.count(), 2) 
+
+
+    def test_available_rooms(self):
+        #available 
+        available_room = Booking.objects.create(user = self.user, room = self.room, check_in_date = date(2026, 3, 3), check_out_date = date(2026, 3, 10), booking_status = "available")
+            #not sure how to structure this fully ?? will need to properly add later when checking available rooms is possible
+
+
 
 #CHECK ALL OF THESE ARE ACTUALLY OK!!!
     
