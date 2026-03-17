1624cb03ddcdaff10ad893adee4adca0856c6cf3
Anjali Yadav <anjaliyadav@Anjalis-MacBook-Air.local>
2026-03-17 20:46:04 +0000

    Remove startup admin creation to fix deployment

--- accounts/apps.py
+++ accounts/apps.py
@@ -3,15 +3,3 @@ from django.apps import AppConfig
 class AccountsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'accounts'
-
-    def ready(self):
-        from .models import create_default_admin, create_default_guest
-        create_default_admin()
-        create_default_guest()
-
-# from django.apps import AppConfig
-
-
-# class AccountsConfig(AppConfig):
-    # default_auto_field = 'django.db.models.BigAutoField'
-    # name = 'accounts'
