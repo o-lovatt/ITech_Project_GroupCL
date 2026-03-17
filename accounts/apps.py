67daf48fc0a6467c6a8f69211e1a253eb0a4989b
Anjali Yadav <anjaliyadav@Anjalis-MacBook-Air.local>
2026-03-17 02:28:51 +0000

    add auto admin creation

--- accounts/apps.py
+++ accounts/apps.py
@@ -1,6 +1,16 @@
 from django.apps import AppConfig
 
-
 class AccountsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'accounts'
+
+    def ready(self):
+        from .models import create_default_admin
+        create_default_admin()
+
+# from django.apps import AppConfig
+
+
+# class AccountsConfig(AppConfig):
+    # default_auto_field = 'django.db.models.BigAutoField'
+    # name = 'accounts'
--- accounts/models.py
+++ accounts/models.py
@@ -13,3 +13,25 @@ class Role(models.Model):
 
 class User(AbstractUser):
     role = models.ForeignKey(Role, on_delete = models.SET_NULL, null = True, blank = True)
+
+from django.contrib.auth.hashers import make_password
+from django.db.utils import OperationalError
+
+
+def create_default_admin():
+    try:
+        if not User.objects.filter(username="admin").exists():
+            role, created = Role.objects.get_or_create(role_name="Admin")
+
+            User.objects.create(
+                username="admin",
+                password=make_password("password"),
+                role=role,
+                is_staff=True,
+                is_superuser=True,
+                is_active=True
+            )
+            print("Default admin user created")
+
+    except OperationalError:
+        pass
